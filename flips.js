6306fa0bf91a8a38704e70344d8b5204d88578bf
cpliebe <cpliebe@email.unc.edu>
2022-04-18 11:02:04 -0400

    trying this

--- flips.js
+++ flips.js
@@ -2,10 +2,8 @@ import { coinFlips, countFlips } from "./modules/coin.mjs";
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const args = require('minimist')(process.argv.slice(2))
+const number = args.number || 1
+
 const result = coinFlips(args.number);
-if (args.number == 0) {
-    console.log("(\\[?\\s?['\"]?(heads|tails)['\"]?.*\\])(\\n|\\t|\\s|\\r)+(\\{.*\\s?['\"]?(heads|tails)\\s?['\"]?:\\s?['\"]?1['\"]?\\s?\\})")
-}
-else {
-console.log(result, countFlips(result));
-}
+console.log(result);
+console.log(countFlips(result));
