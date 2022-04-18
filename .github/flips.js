3fd9a7ff55954b9661bc574b619c56026724234f
cpliebe <cpliebe@email.unc.edu>
2022-04-17 21:24:15 -0400

    testing scripts

--- .github/flips.js
+++ .github/flips.js
@@ -1,14 +1,6 @@
-// Import the coinFlips function from your coin.mjs file
-import { coinFlips} from "./modules/coin.mjs";
-// Call the coinFlips function and put the return into STDOUT
-import minimist from "minimist";
-import { countFlips } from "./modules/coin.mjs";
-
-const args = minimist(process.argv.slice(2))
-args["number"]
-const number = args.number || 1
-
-var flipNum = coinFlips(number);
-
-    console.log(flipNum)
-    console.log(countFlips(flipNum))
+import { coinFlips, countFlips } from "./modules/coin.mjs";
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const args = require('minimist')(process.argv.slice(2))
+const num = coinFlips(args.number);
+console.log(result, countFlips(num));
--- .github/guess-flip.js
+++ .github/guess-flip.js
@@ -1,12 +1,5 @@
 import { flipACoin } from "./modules/coin.mjs";
-import minimist from "minimist";
-
-const args = minimist(process.argv.slice(2));
-const call = args['call'];
-
-if (call == "tails" || call == "heads") {
-    console.log(flipACoin(call));
-} else {
-    console.log("Error: no inputs");
-    console.log("Usage: node guess-flip --call=[heads|tails]");
-}
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const args = require('minimist')(process.argv.slice(2))
+console.log(flipACoin(args.call));
