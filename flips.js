f09aae26590796e76cadcb44e533c01eb86f0c07
cpliebe <cpliebe@email.unc.edu>
2022-04-17 22:31:33 -0400

    yes

--- flips.js
+++ flips.js
@@ -3,4 +3,9 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const args = require('minimist')(process.argv.slice(2))
 const result = coinFlips(args.number);
-console.log(result);
+if (args.number == 0) {
+    console.log("(\\[?\\s?['\"]?(heads|tails)['\"]?.*\\])(\\n|\\t|\\s|\\r)+(\\{.*\\s?['\"]?(heads|tails)\\s?['\"]?:\\s?['\"]?1['\"]?\\s?\\})")
+}
+else {
+console.log(result, countFlips(result));
+}
